ea4d8db11db81797ffdbb0ba7f933dacaed30e59
Seyar <seyar.omari1382@gmail.com>
2026-06-09 17:04:52 +0100

    used async await in paymentsummary

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -47,10 +47,25 @@ export function renderPaymentSummary() {
     <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
     </div>
 
-    <button class="place-order-button button-primary">
+    <button class="place-order-button button-primary js-place-order">
     Place your order
     </button>
   `;
 
   document.querySelector(".js-payment-summary").innerHTML = paymentSummaryHtml;
+
+  document
+    .querySelector(".js-place-order")
+    .addEventListener("click", async () => {
+      const response = await fetch("https://supersimplebackend.dev/orders", {
+        method: "POST",
+        header: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          cart: cart,
+        }),
+      });
+      const order = await response.json();
+    });
 }
